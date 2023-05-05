8103311e2088fa9552ed114a863f6db53e1c6630
Thomas-Christie <thomashamish@hotmail.com>
2023-05-05 09:25:52 +0100

    Added results on LSQ problem using RBF kernel

--- trieste/al_experiment_runner.py
+++ trieste/al_experiment_runner.py
@@ -23,13 +23,13 @@ EQUALITY_CONSTRAINT_TWO = "EQUALITY_CONSTRAINT_TWO"
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_integer('num_experiments', 50, 'Number of repeats of experiment to run.')
-flags.DEFINE_integer('num_bo_iterations', 140, 'Number of iterations of Bayesian optimisation to run for.')
+flags.DEFINE_integer('num_experiments', 100, 'Number of repeats of experiment to run.')
+flags.DEFINE_integer('num_bo_iterations', 40, 'Number of iterations of Bayesian optimisation to run for.')
 flags.DEFINE_float('epsilon', 0.01, 'Bound within which equality constraints are considered to be satisfied.')
-flags.DEFINE_enum('problem', 'GSBP', ['LSQ', 'GSBP', 'LOCKWOOD'], 'Test problem to use.')
+flags.DEFINE_enum('problem', 'LSQ', ['LSQ', 'GSBP', 'LOCKWOOD'], 'Test problem to use.')
 flags.DEFINE_integer('num_rff_features', 1000, 'Number of Random Fourier Features to use when approximating the kernel.')
 flags.DEFINE_integer('batch_size', 1, 'Number of points to sample at each iteration of BO.')
-flags.DEFINE_integer('num_initial_samples', 10, 'Number of random samples to fit models before starting BO.')
+flags.DEFINE_integer('num_initial_samples', 5, 'Number of random samples to fit models before starting BO.')
 flags.DEFINE_boolean('update_lagrange_via_kkt', False, 'Whether to update Lagrange multipliers using a gradient-based'
                                                        'approach based on KKT conditions.')
 flags.DEFINE_boolean('conservative_penalty_decrease', False, 'Whether to reduce the penalty parameter more '
@@ -47,7 +47,7 @@ flags.DEFINE_integer('num_acquisition_optimiser_start_points', 5000, 'Number of 
 flags.DEFINE_boolean('known_objective', False, 'Whether to use a known objective function or model it with a surrogate.')
 flags.DEFINE_enum('kernel_name', 'squared_exponential', ['matern52', 'squared_exponential'], 'Which kernel to use.')
 flags.DEFINE_boolean('save_lagrange', True, 'Save intermediate values of Lagrange multipliers.')
-flags.DEFINE_string('save_path', 'results/final_ts_results/gsbp/adam_no_prev_rbf_aggressive/data/run_',
+flags.DEFINE_string('save_path', 'results/final_ts_results/lsq/adam_no_prev_rbf/data/run_',
                     'Prefix of path to save results to.')
 
 
